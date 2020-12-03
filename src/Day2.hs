2658dbce60c6eb58a74250be6f6eb69716d982ea
Anthony Brice <anthonymbrice@gmail.com>
2020-12-02 18:34:26 -0800

    x

--- src/Day2.hs
+++ src/Day2.hs
@@ -1,24 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Day2
   ( day2
   ) where
 
-import Data.Text (pack, splitOn, unpack)
+import qualified Data.Text as T
+import Data.Text.Read (decimal)
+import Data.Either (rights)
 
 day2 = do
   content <- readFile "input/d2"
-  let raw = lines content
+  let raw = map T.pack $ lines content
       xs = map f raw
   print $ length $ filter valid xs
   print $ length $ filter valid' xs
 
   where
-    f x = let [y1, y2, y3] = words x
-              [p,q] = map (read . unpack) $ splitOn (pack "-") $ pack y1
-          in ((p,q), y2!!0, y3)
+    f x = let [y1, y2, y3] = T.words x
+              (p,_):(q,_):_ = rights $ map decimal $ T.splitOn "-" y1
+          in ((p,q), T.index y2 0, y3)
 
     valid ((p,q), c, pw) =
-      let n = length $ filter (== c) pw
+      let n = T.length $ T.filter (== c) pw
       in p <= n && n <= q
 
-    valid' ((p,q), c, pw) = (pw!!(p-1) == c || pw!!(q-1) == c)
-      && not (pw!!(p-1) == c && pw!!(q-1) == c)
+    valid' ((p,q), c, pw) =
+      (g p' == c || g q' == c) && (g p' /= c || g q' /= c)
+      where p' = p-1; q' = q-1; g = T.index pw
